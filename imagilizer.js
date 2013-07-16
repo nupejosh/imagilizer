690559c6af4db2d4b5ce98de6ce1cfbfe6524168
MakerSquare Student <students@themakersquare.com>
2013-07-16 11:59:30 -0500

    I've started working on the Imagilizer and laid the foundation. The next step is to figure out how to fade in/out  on click

--- imagilizer.js
+++ imagilizer.js
@@ -1,1 +1,19 @@
-alert("Yolo, I said YOLO")
+// $(document).ready( function (){
+// 	$("img:nth-child(1)").addClass('active');
+// 	$("img:nth-child(1)").clone().removeAttr('width').appendTo('#panel');
+// 	$("#sidebar img").click(function (){})
+
+// })
+
+$(document).ready( function(){
+	$("#sidebar img:nth-child(n)").click(function(){
+		$(this).addClass('active');
+		$(this).clone().removeAttr('width').appendTo('#panel');
+	})
+	// if ($('#sidebar img:nth-child(n):not(.active)').click()){
+	// 	$("#panel img:nth-child(n)").fadeOut();
+	// 	$(this).addClass('active');
+	// 	$(this).clone().removeAttr('width').appendTo('#panel');
+		
+	// };
+})
